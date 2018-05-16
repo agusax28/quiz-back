6a8f654504352a8d0f1d5d330def206a502772b9
AgustinPM <agusax28@gmail.com>
2018-05-16 19:26:28 +0200

    Mapeo resultado

---
+++ src/main/java/com/apm/quizback/model/Result.java
@@ -0,0 +1,37 @@
+package com.apm.quizback.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Result {
+
+	public static final String FIELD_COURSE = "course";
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	private float score;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date date;
+
+	@JoinColumn(name = FIELD_COURSE)
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Course course;
+
+}
