e16a97bf0793342b5926ec3517cef1f9dd142c6d
AgustinPM <agusax28@gmail.com>
2018-05-16 19:47:44 +0200

    Mapeo tag

--- src/main/java/com/apm/quizback/model/Tag.java
+++ src/main/java/com/apm/quizback/model/Tag.java
@@ -4,13 +4,11 @@ import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -18,9 +16,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
-public class Cuestionary {
-
-	public static final String FIELD_COURSE = "course";
+public class Tag {
 	
 	@Id
 	@GeneratedValue
@@ -29,14 +25,10 @@ public class Cuestionary {
 	@Column(nullable = false)
 	private String name;
 	
-	@JoinColumn(name = FIELD_COURSE)
-	@ManyToOne(fetch = FetchType.LAZY)
-	private Course course;
-	
 	@ManyToMany
 	@JoinTable (name= "tag_cuestionary",
-			joinColumns= {@JoinColumn(name= "idCuestionary")},
-			inverseJoinColumns= {@JoinColumn(name= "idTag")})
+			joinColumns= {@JoinColumn(name= "idTag")},
+			inverseJoinColumns= {@JoinColumn(name= "idCuestionary")})
 	private List<Cuestionary> cuestionary;
-	
+
 }
