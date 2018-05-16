a6e58a764f6101e9dd9d152ee70a4caa31c05417
AgustinPM <agusax28@gmail.com>
2018-05-16 19:19:38 +0200

    Mapeo Curso

---
+++ src/main/java/com/apm/quizback/model/Course.java
@@ -0,0 +1,39 @@
+package com.apm.quizback.model;
+
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Course {
+	
+	@Id
+	@GeneratedValue
+	private Integer id;
+	
+	@Column(nullable = false)
+	private String name;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = Result.FIELD_COURSE)
+	private List<Result> result;
+	
+	@ManyToMany
+	@JoinTable (name= "user_course",
+			joinColumns= {@JoinColumn(name= "idCourse")},
+			inverseJoinColumns= {@JoinColumn(name= "idUser")})
+	private List<Course> course;
+	
+}
