d92b12bc85929114d1faa2c25fad6ab052ea684f
AgustinPM <agusax28@gmail.com>
2018-05-21 16:54:03 +0200

    UserDAO

---
+++ src/main/java/com/apm/quizback/dao/UserDAO.java
@@ -0,0 +1,25 @@
+package com.apm.quizback.dao;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import com.apm.quizback.model.User;
+
+@Repository
+public interface UserDAO extends PagingAndSortingRepository<User, Integer>{
+
+	//Todos
+	Optional<User> findOrderByNameDesc();
+	
+	//Por nombre
+	Optional<User> findOneByNameOrderByIdUserDesc(String name);
+	
+	//Por email
+	Optional<User> findOneByEmail(String email);
+	
+	//Login email+pass
+	Optional<User> findOneByEmailAndPassword(String email, String password);
+	
+}
