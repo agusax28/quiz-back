37bbbacbccbd627a932f83351db64e24504e3309
AgustinPM <agusax28@gmail.com>
2018-05-21 16:55:11 +0200

    CourseDAO

---
+++ src/main/java/com/apm/quizback/dao/CourseDAO.java
@@ -0,0 +1,18 @@
+package com.apm.quizback.dao;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import com.apm.quizback.model.Course;
+
+public interface CourseDAO extends PagingAndSortingRepository<Course, Integer>{
+	
+	//Todos
+	Optional<Course> findOrderByIdCourseDesc();
+	
+	//1
+	Optional<Course> findOneByIdCourse(int id);
+
+}
