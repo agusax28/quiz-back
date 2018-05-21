a297caa13863c569e543c7024314b898c87b5bf3
AgustinPM <agusax28@gmail.com>
2018-05-21 16:55:32 +0200

    DifficultyDAO

---
+++ src/main/java/com/apm/quizback/dao/DifficultyDAO.java
@@ -0,0 +1,21 @@
+package com.apm.quizback.dao;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import com.apm.quizback.model.Difficulty;
+
+public interface DifficultyDAO extends PagingAndSortingRepository<Difficulty, Integer>{
+	
+	//Todos
+	Optional<Difficulty> findOrderByIdDifficultyDesc();
+	
+	//1
+	Optional<Difficulty> findOneByIdDifficulty(int id);
+	
+	//Nombre
+	Optional<Difficulty> findOneByName(String name);
+
+}
