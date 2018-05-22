293e5361b5e26a3e5a00c38f848e0ee5afd58938
AgustinPM <agusax28@gmail.com>
2018-05-22 15:07:44 +0200

    Controlador usuario

--- src/main/java/com/apm/quizback/controller/UserController.java
+++ src/main/java/com/apm/quizback/controller/UserController.java
@@ -1,5 +1,13 @@
 package com.apm.quizback.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.apm.quizback.service.UserService;
+
+@Controller
 public class UserController {
 
+	@Autowired
+	UserService userService;
 }
