0f2a94e1b03dc7ac6887aeb59809be96ea3226d0
AgustinPM <agusax28@gmail.com>
2018-05-22 22:42:19 +0200

    UserController haciendo uso de usermapper

--- src/main/java/com/apm/quizback/controller/UserController.java
+++ src/main/java/com/apm/quizback/controller/UserController.java
@@ -1,13 +1,43 @@
 package com.apm.quizback.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apm.quizback.component.mapper.user.UserMapper;
+import com.apm.quizback.dto.UserDTO;
+import com.apm.quizback.dto.UserPostDTO;
+import com.apm.quizback.model.User;
 import com.apm.quizback.service.UserService;
 
-@Controller
+@RestController
+@RequestMapping(value = "/user")
 public class UserController {
 
 	@Autowired
 	UserService userService;
+
+	@Autowired
+	UserMapper userMapper;
+
+	@RequestMapping(method = RequestMethod.GET)
+	public Set<UserDTO> findAll(@RequestParam(defaultValue = "0", required = false) Integer page,
+			@RequestParam(defaultValue = "10", required = false) Integer size) {
+		final Set<User> users = userService.findAll(PageRequest.of(page, size));
+		return userMapper.modelToDto(users);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public UserDTO create(@RequestBody UserPostDTO dto) {
+		final User user = userMapper.dtoToModel(dto);
+		final User createUser = userService.create(user);
+		return userMapper.modelToDto(createUser);
+	}
+
 }
