df80acbbfa3d799d4c7f2bc9cedeae3c92bdd85c
AgustinPM <agusax28@gmail.com>
2018-05-26 19:44:26 +0200

    Añadido excepciones en el abstracto

--- src/main/java/com/apm/quizback/service/AbstractCRUDService.java
+++ src/main/java/com/apm/quizback/service/AbstractCRUDService.java
@@ -6,16 +6,21 @@ import java.util.Set;
 
 import org.springframework.data.domain.Pageable;
 
+import com.apm.quizback.exception.InvalidDataException;
+import com.apm.quizback.exception.NotFoundException;
+
 public interface AbstractCRUDService<T, ID extends Serializable> {
 
-	T create(T t);
+	T create(T t) throws InvalidDataException;
 
-	void update(T t);
+	void update(T t) throws InvalidDataException;
 
-	Optional<T> findById(ID id);
+	Optional<T> findById(ID id) throws NotFoundException;
 
 	Set<T> findAll(Pageable p);
 
 	void delete(T t);
+	
+	boolean validate(T t);
 
 }
