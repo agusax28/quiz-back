68e8bb2799ed76496b121a0b05b6a8ee497d3167
AgustinPM <agusax28@gmail.com>
2018-05-26 15:58:31 +0200

    UserController arreglos en update

--- src/main/java/com/apm/quizback/controller/UserController.java
+++ src/main/java/com/apm/quizback/controller/UserController.java
@@ -5,8 +5,8 @@ import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,11 +31,18 @@ public class UserController {
 	@Autowired
 	UserMapper userMapper;
 
-	@RequestMapping(method = RequestMethod.GET)
+	// @RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public Set<UserDTO> findAll(@RequestParam(defaultValue = "0", required = false) Integer page,
 			@RequestParam(defaultValue = "10", required = false) Integer size) {
 		final Set<User> users = userService.findAll(PageRequest.of(page, size));
 		return userMapper.modelToDto(users);
+	}
+
+	@GetMapping("/{id}")
+	public UserDTO findById(@PathVariable Integer id) {
+		final Optional<User> user = userService.findById(id);
+		return userMapper.modelToDto(user.get());
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
@@ -45,16 +52,20 @@ public class UserController {
 		return userMapper.modelToDto(createUser);
 	}
 
-	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-    public void update(@RequestBody UserPostDTO dto) {
+	// Pasar ID por pathvariable, arreglar aqui
+	// @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	@PutMapping("/{id}")
+	public void update(@PathVariable Integer id, @RequestBody UserPostDTO dto) {
 		final User user = userMapper.dtoToModel(dto);
-        userService.update(user);
-    }
-	
-	@RequestMapping(value = "/{idUser}", method = RequestMethod.DELETE)
+		user.setIdUser(id);
+		userService.update(user);
+	}
+
+	// @RequestMapping(value = "/{idUser}", method = RequestMethod.DELETE)
+	@DeleteMapping("/{idUser}")
 	public void delete(@PathVariable Integer idUser) {
 		final Optional<User> user = userService.findById(idUser);
 		userService.delete(user.get());
 	}
-	
+
 }
