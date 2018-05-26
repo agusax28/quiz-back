9de2a3d975b4645b1b51bfc39bb8e8a733030c6d
AgustinPM <agusax28@gmail.com>
2018-05-26 13:32:22 +0200

    Renombrado

---
+++ src/main/java/com/apm/quizback/dao/QuestionaryDAO.java
@@ -0,0 +1,18 @@
+package com.apm.quizback.dao;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import com.apm.quizback.model.Questionary;
+
+public interface QuestionaryDAO extends PagingAndSortingRepository<Questionary, Integer>{
+	
+	//1
+	Optional<Questionary> findOneByIdQuestionary(int id);
+	
+	//Nombre
+	Optional<Questionary> findOneByName(String name);
+
+}
