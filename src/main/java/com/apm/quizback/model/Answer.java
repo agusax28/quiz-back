e3f9ae896d7fc13ce58f3ae916c1add0d9605492
AgustinPM <agusax28@gmail.com>
2018-05-28 16:43:15 +0200

    Fixed

--- src/main/java/com/apm/quizback/model/Answer.java
+++ src/main/java/com/apm/quizback/model/Answer.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -19,7 +20,7 @@ public class Answer {
 	public static final String FIELD_QUESTION = "question";
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer idAnswer;
 	
 	@Column(nullable = false)
