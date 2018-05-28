df08ad41649b8c6679093285923019d293b9c800
AgustinPM <agusax28@gmail.com>
2018-05-28 19:47:19 +0200

    Test UserMapper v1

---
+++ src/test/java/com/apm/quizback/component/mapper/user/TestUserMapper.java
@@ -0,0 +1,42 @@
+package com.apm.quizback.component.mapper.user;
+
+import org.dozer.DozerBeanMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import com.apm.quizback.dto.UserDTO;
+import com.apm.quizback.model.User;
+
+import junit.framework.Assert;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestUserMapper {
+	
+	private static final String NAME = "Pepe";
+	
+	@InjectMocks
+	UserMapper mapper = new UserMapperImpl();
+	
+	@Mock
+	DozerBeanMapper dozer;
+	
+	/*public void testModelToDTO() {
+		
+	}*/
+	
+	@Test
+	public void testDtoToModel() {
+		final UserDTO userDTO = new UserDTO();
+		userDTO.setName(NAME);
+		final User user = new User();
+		user.setName(NAME);
+		Mockito.when(dozer.map(userDTO, User.class)).thenReturn(user);
+		final User res = mapper.dtoToModel(userDTO);
+		Assert.assertEquals(NAME, res.getName());
+	}
+
+}
